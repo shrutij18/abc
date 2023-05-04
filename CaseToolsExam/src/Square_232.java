1649ad24967ce5875923071bfa1378397c2554db
exam <exam@Bcalab4-m09>
2023-05-04 12:33:23 +0530

    first commit

---
+++ CaseToolsExam/src/Square_232.java
@@ -0,0 +1,13 @@
+
+public class Square_232 {
+   int length;
+   Square_232 (int length)
+   {
+	   this.length=length;
+   }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
