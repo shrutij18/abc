3ef3c6502350e41b44ce550254412872d36a6a2a
exam <exam@Bcalab4-m09>
2023-05-04 12:37:11 +0530

    ve

--- CaseToolsExam/src/Square_232.java
+++ CaseToolsExam/src/Square_232.java
@@ -1,9 +1,13 @@
 
 public class Square_232 {
    int length;
-   Square_232 (int length)
+   int area;
+   int perimeter;
+   Square_232 (int length,int area, int perimeter)
    {
 	   this.length=length;
+	   this.area=area;
+	   this.perimeter=perimeter;
    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
